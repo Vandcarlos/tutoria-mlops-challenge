be65754b08c1d087246231ed3e716fef87e0f7b0
Vandcarlos Mouzinho Sandes Junior <vandcarlosjunior@gmail.com>
2025-12-04 03:19:20 -0300

    Update network.tf

--- infra/envs/prod/api/network.tf
+++ infra/envs/prod/api/network.tf
@@ -3,7 +3,7 @@
 // Responsible for creating the security group used by MLflow ECS tasks.
 
 module "sg_api" {
-  source = "../sg-app"
+  source = "../../../modules/sg-app"
 
   name_prefix         = local.name_prefix
   vpc_id              = var.vpc_id
