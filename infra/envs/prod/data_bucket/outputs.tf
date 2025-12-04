9c519e6369f35f8b6fffe0839e4ab3f5b3a1a586
Vandcarlos Mouzinho Sandes Junior <vandcarlosjunior@gmail.com>
2025-12-04 03:39:07 -0300

    Update outputs.tf

--- infra/envs/prod/data_bucket/outputs.tf
+++ infra/envs/prod/data_bucket/outputs.tf
@@ -1,6 +1,6 @@
 output "data_bucket_name" {
   description = "Name of the S3 data bucket"
-  value       = module.data_bucket.bucket
+  value       = module.data_bucket.bucket_name
 }
 
 output "data_bucket_arn" {
